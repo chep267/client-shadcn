bd09dc9b079968376f5e30850203dd0c03f0b1d1
dongntd <dong.nguyenthanh@powergatesoftware.com>
2026-05-07 10:15:09 +0700

    feat: update code

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,6 @@
 /** libs */
 import { mergeConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 /** config */
 import viteConfig from './vite.config';
@@ -15,7 +14,6 @@ import viteConfig from './vite.config';
 export default mergeConfig(
     viteConfig({ mode: 'test', command: 'build' }),
     defineConfig({
-        plugins: [tsconfigPaths()],
         test: {
             globals: true,
             environment: 'jsdom',
